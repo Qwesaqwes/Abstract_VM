07d5e965fc98e59d677ef0915efaf3b152bc7cd9
Jimmy CHEN-MA <jichen-m@e3r7p21.42.fr>
2018-04-27 18:33:54 +0200

    erase commentary in main

--- main.cpp
+++ main.cpp
@@ -6,45 +6,17 @@
 /*   By: jichen-m <jichen-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 16:17:01 by jichen-m          #+#    #+#             */
-/*   Updated: 2018/04/26 16:02:46 by jichen-m         ###   ########.fr       */
+/*   Updated: 2018/04/27 18:33:06 by jichen-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./srcs/Parse.hpp"
-// #include "Parse.cpp"
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
-	{
 		Parse	Parse(av[1]);
-
-
-		// std::vector<std::string> content;
-		// content = Parse.getVector();
-		// std::cout << std::endl;
-		// for(unsigned long i = 0; i < content.size(); i++)
-		// {
-		// 	std::cout << content[i] << std::endl;
-		// }
-	}
 	else
-	{
 		Parse	Parse;
-
-
-		// std::vector<std::string> content;
-		// content = Parse.getVector();
-		// std::cout << std::endl;
-		// for(unsigned long i = 0; i < content.size(); i++)
-		// {
-		// 	std::cout << content[i] << std::endl;
-		// }
-	}
-
-
-
 	return (0);
 }
-
-/*only for display need to erase later*/
